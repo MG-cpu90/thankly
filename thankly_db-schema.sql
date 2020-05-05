666b8b4229f35a5564e20799b567812e58561aaf
Massimo Guida <massimo.guidamusic@gmail.com>
2020-05-05 19:11:09 -0400

    add template folders

---
+++ thankly_db-schema.sql
@@ -0,0 +1,16 @@
+### Schema
+-- Drops the database if it exists currently --
+DROP DATABASE IF EXISTS `sequelize_thankly_db`;
+
+-- Creates the database --
+CREATE DATABASE `sequelize_thankly_db`;
+USE `sequelize_thankly_db`;
+
+-- CREATE TABLE thanks
+-- (
+-- 	id int NOT NULL AUTO_INCREMENT,
+-- 	author varchar(255) NOT NULL,
+-- 	body TEXT NOT NULL,
+--     date TEXT NOT NULL,
+-- 	PRIMARY KEY (id)
+-- );
