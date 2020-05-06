cbf53ac2cdefdaf54c086d4ec2164abea6fe6f55
Massimo Guida <massimo.guidamusic@gmail.com>
2020-05-06 17:29:02 -0400

    added routes, modified html, and js

--- public/js/signup.js
+++ public/js/signup.js
@@ -45,21 +45,72 @@ $(document).ready(function() {
 $(document).ready(function() {
   // Getting references to our form and input
   var thankForm = $("form.login");
-  var authorInput = $("input#email-input");
-  var bodyInput = $("input#password-input");
+  var authorInput = $("input#email-input2");
+  var bodyInput = $("input#password-input2");
   thankForm.on("submit", function(event) {
     event.preventDefault();
     console.log("clicked");
-    // var userPost = {
-    //   author: authorInput.val().trim(),
-    //   body: bodyInput.val().trim()
-    // };
-    // if (!userPost.author || !userPost.body) {
-    //   return;
-    // }
+
+    // GET request
     $.get("/api/prompt", function(data) {
       console.log(data);
     
     });
+
+    // Make a userPost object
+    var userPost = {
+      author: authorInput.val().trim(),
+      body: bodyInput.val().trim(),
+      created_at: moment().format("YYYY-MM-DD HH:mm:ss")
+    };
+    if (!userPost.author || !userPost.body) {
+      return;
+    }
+
+  // Send an AJAX POST-request with jQuery
+  $.post("/api/new", userPost)
+    // On success, run the following code
+    .then(function() {
+      console.log(userPost);
+
+      var row = $("<div>");
+      row.addClass("thank");
+
+      row.append("<p>" + userPost.author + " posted: </p>");
+      row.append("<p>" + userPost.body + "</p>");
+      row.append("<p>At " + moment(userPost.created_at).format("h:mma on dddd, MMMM Do YYYY") + "</p>");
+      row.append("<button><i class='fa fa-trash'></i></button>");
+
+      $("#thank-area").prepend(row);
+
+    });
+
+  // Empty each input box by replacing the value with an empty string
+  authorInput.val("");
+  bodyInput.val("");
+
 });
 })
+
+// When the page loads, grab all of our thanks
+$.get("/api/all", function(data) {
+
+  if (data.length !== 0) {
+
+    for (var i = 0; i < data.length; i++) {
+
+      var row = $("<div>");
+      row.addClass("thank");
+
+      row.append("<p>" + data[i].author + " posted: </p>");
+      row.append("<p>" + data[i].body + "</p>");
+      row.append("<p>At " + moment(data[i].created_at).format("h:mma on dddd, MMMM Do YYYY") + "</p>");
+      row.append("<button><i class='fa fa-trash'></i></button>");
+
+      $("#thank-area").prepend(row);
+
+    }
+
+  }
+
+});
